d5ff5b5f9bfa41d740c1ff1b2c3292e9c954ff1c
ihsan <ihsan.mujdeci@presien.com>
2021-09-16 17:46:35 +1000

    lol

--- src/servid/rest.ts
+++ src/servid/rest.ts
@@ -5,12 +5,12 @@ export function startServer(port: number): Promise<express.Application> {
 
   return new Promise((resolve, reject) => {
     app.on;
+
     app
       .listen(port, () => {
         console.log(`Server listening at http://localhost:${port}`);
         resolve(app);
       })
-
       .on("error", reject);
   });
 }
