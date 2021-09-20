8ac51a2f2b77d80c4f10e40404b3d3b88c46d710
ihsan <ihsan.mujdeci@presien.com>
2021-09-20 14:08:24 +1000

    update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,11 @@
-import {add} from "@myApp/index";
+import { add } from "@myApp/index";
 
-describe("Index test", ()=>{
-    it("Should be able to add 1 and 1", ()=>{
-        expect(add(1,1)).toEqual(2)
-    })
+describe("Index test", () => {
+  it("Should be able to add 1 and 1", () => {
+    expect(add(1, 1)).toEqual(2);
+  });
 
-    it("Should load environment variable", ()=>{
-        expect(process.env.HELLO).toEqual("world")
-    })
+  it("Should load environment variable", () => {
+    expect(process.env.HELLO).toEqual("testing");
+  });
 });
