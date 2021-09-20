c2925b18a53f0a379cdb15c6ee2ee3b7c45fbfb4
ihsan <ihsan.mujdeci@presien.com>
2021-09-20 13:37:56 +1000

    properly implement master

--- env.load.js
+++ env.load.js
@@ -5,7 +5,6 @@ try {
   fs.readFileSync(process.env.DOTENV_CONFIG_PATH);
 } catch (e) {
   throw new Error(
-    process.env.DOTENV_CONFIG_PATH,
-    "file not found in root directory."
+    process.env.DOTENV_CONFIG_PATH + " file not found in root directory."
   );
 }
