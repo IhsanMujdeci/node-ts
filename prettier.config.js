269a4510df33467673c7ecf0b2413393e9dbd47a
Ihsan Mujdeci <ihsanmujdeci.im@gmail.com>
2023-03-15 08:28:41 +1100

    remove need for dependency

--- prettier.config.js
+++ prettier.config.js
@@ -1,17 +1,24 @@
-const requireJSON5 = require('require-json5');
-const tsConfig = requireJSON5('./tsconfig.json');
+// Get all paths from compilerOptions.paths and format them in a regex that targets any imports starting with them
+function regexPathsFromTsConfig(tsConfigPath) {
+  const stripCommentsRegex = /\\"|"(?:\\"|[^"])*"|(\/\/.*|\/\*[\s\S]*?\*\/)/g;
+  let rawConfig = require('fs').readFileSync(tsConfigPath, { encoding: 'utf8' });
+  let strippedConfig = rawConfig.replace(stripCommentsRegex, (m, g) => (g ? '' : m));
+  let config = JSON.parse(strippedConfig);
+  const regexKeys = Object.keys(config.compilerOptions.paths)
+    .map(k => k.replace('*', ''))
+    .join('|');
+  return '^(' + regexKeys + ')';
+}
 
-// Get keys of tsconfig path aliases.
-const regPaths = Object.keys(tsConfig.compilerOptions.paths)
-  .map(k => k.replace('*', ''))
-  .join('|');
+// Any imports that start with a "." or ".."
+const relativeImportsRegex = '^\\.{1,2}';
 
 module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
   printWidth: 100,
-  importOrder: ['^(' + regPaths + ')', '^\\.{1,2}'],
+  importOrder: [regexPathsFromTsConfig('./tsconfig.json'), relativeImportsRegex],
   importOrderSortSpecifiers: true,
   importOrderSeparation: true,
 };
