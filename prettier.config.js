80f7eeefe6dbf83950fad07e14e4bd36c4956ccf
Ihsan Mujdeci <ihsanmujdeci.im@gmail.com>
2023-03-14 13:21:29 +1100

    properly fix .. and . imports

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 const requireJSON5 = require('require-json5');
 const tsConfig = requireJSON5('./tsconfig.json');
 
+// Get keys of tsconfig path aliases.
 const regPaths = Object.keys(tsConfig.compilerOptions.paths)
   .map(k => k.replace('*', ''))
   .join('|');
@@ -10,7 +11,7 @@ module.exports = {
   trailingComma: 'all',
   arrowParens: 'avoid',
   printWidth: 100,
-  importOrder: ['^(' + regPaths + ')'],
+  importOrder: ['^(' + regPaths + ')', '^\\.{1,2}'],
   importOrderSortSpecifiers: true,
   importOrderSeparation: true,
 };
