f7bf7f76a8f846c3e00709afb16f85e610c30265
Ihsan Mujdeci <ihsanmujdeci.im@gmail.com>
2023-03-22 12:26:03 +1100

    adapt to not creash on no regex paths

--- prettier.config.js
+++ prettier.config.js
@@ -9,21 +9,28 @@ function regexPathsFromTsConfig(tsConfigPath) {
     .replace(stripTrailingComma, (m, g) => (g ? '' : m));
 
   let config = JSON.parse(strippedConfig);
+  if (!config.compilerOptions || !config.compilerOptions.paths) {
+    return null;
+  }
   const regexKeys = Object.keys(config.compilerOptions.paths)
     .map(k => k.replace('*', ''))
     .join('|');
   return '^(' + regexKeys + ')';
 }
 
-// Any imports that start with a "." or ".."
-const relativeImportsRegex = '^\\.{1,2}';
+const importOrder = [];
+const pathRegex = regexPathsFromTsConfig('./tsconfig.json');
+if (pathRegex) {
+  const relativeImportsRegex = '^\\.{1,2}';
+  importOrder.push(pathRegex, relativeImportsRegex);
+}
 
 module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
   printWidth: 100,
-  importOrder: [regexPathsFromTsConfig('./tsconfig.json'), relativeImportsRegex],
+  importOrder: importOrder,
   importOrderSortSpecifiers: true,
   importOrderSeparation: true,
 };
